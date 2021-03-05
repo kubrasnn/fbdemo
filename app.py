e1a43d3bc921882b963ddc157434dda6c816ba59
Kübra Sinan <kubra.sinan@metro.istanbul>
2021-03-05 15:03:11 +0300

    initial commit of basic flask server

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Merhaba Dünyalı"
+
+@app.route('/hi')
+def hi():
+    return "Merhaba Kübra"
+
+if __name__ == '__main__' :
+    app.run(debug=True)
+    
